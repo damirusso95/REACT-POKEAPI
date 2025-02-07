e97e136ad8f0bbad516f1866f931bebdb68bf25c
damirusso95 <damirusso95@gmail.com>
2025-02-07 12:32:03 +0100

    componente pokemon dettaglio

---
+++ src/components/PokemonDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PokemonDetail() {
+    return (
+        <div className="pokemon-detail">
+            <h1>Dettagli del Pokémon</h1>
+            <p>Qui verranno mostrati i dettagli del Pokémon.</p>
+        </div>
+    );
+}
+
+export default PokemonDetail;
