386443859045968bea98ea5459e31f8a752ef048
damirusso95 <damirusso95@gmail.com>
2025-02-07 11:39:48 +0100

    funzione per filtrare i nomi

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,4 +1,3 @@
-// hook
 import React, { useEffect, useState } from "react";
 
 function PokemonList() {
@@ -6,16 +5,16 @@ function PokemonList() {
     const [pokemons, setPokemons] = useState([]);
 
     // variabile di stato per la ricerca per nome
-    const [serchName, setSerchName] = useState('');
+    const [searchName, setSearchName] = useState("");
 
     // funzione per API
     useEffect(() => {
-
         const fetchPokemon = async () => {
             const response = await fetch("https://pokeapi.co/api/v2/pokedex/1");
 
             // convertiamo la risposta in json
             const data = await response.json();
+
 
             // aggiorno lo stato della lista pokemon
             setPokemons(data.pokemon_entries);
@@ -24,16 +23,28 @@ function PokemonList() {
         fetchPokemon();
     }, []);
 
+    // Se la barra di ricerca è vuota, mostriamo tutti i Pokémon
+    const filteredPokemons = searchName
+        ? pokemons.filter((pokemon) =>
+            pokemon.pokemon_species.name.toLowerCase().includes(searchName.toLowerCase())
+        )
+        : pokemons;
+
     return (
         <div>
             <h2>Lista Pokémon</h2>
 
-            {/* input per la ricerca nome */}
-            <input type="text" placeholder="Cerca un Pokemon..." value={serchName} onClick={setSerchName} />
+            {/* Barra di ricerca */}
+            <input
+                type="text"
+                placeholder="Cerca un Pokémon..."
+                value={searchName}
+                onChange={(e) => setSearchName(e.target.value)}
+            />
 
-
+            {/* Mostriamo SOLO i Pokémon filtrati o tutti se la ricerca è vuota */}
             <ul>
-                {pokemons.map((pokemon) => (
+                {filteredPokemons.map((pokemon) => (
                     <li key={pokemon.entry_number}>
                         <img
                             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.entry_number}.png`}
