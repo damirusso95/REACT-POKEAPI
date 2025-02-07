c1f47e11022a4d62499968b1ee6f95e7f49f79c5
damirusso95 <damirusso95@gmail.com>
2025-02-07 13:19:06 +0100

    API dettagli singolo pokemon + css

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function PokemonDetails() {
-    return (
-        <div className="pokemon-detail">
-            <h1>Dettagli del Pokémon</h1>
-            <p>Qui verranno mostrati i dettagli del Pokémon.</p>
-        </div>
-    );
+  // useParams() otteniamo l'ID dal URL
+  const { id } = useParams(); 
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const fetchPokemonDetails = async () => {
+      // richiesta all'API con id
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      const data = await response.json();
+      setPokemon(data); 
+    };
+
+    fetchPokemonDetails();
+  }, [id]); 
+
+  if (!pokemon) {
+    return <div>Caricamento...</div>; 
+  }
+  return (
+    <div className="pokemon-detail-card">
+      <h1 className="pokemon-name">{pokemon.name}</h1>
+      <img
+        src={pokemon.sprites.front_default}
+        alt={pokemon.name}
+        className="pokemon-img"
+      />
+      <div className="pokemon-info">
+        <h3>Abilità:</h3>
+        <ul>
+          {pokemon.abilities.map((ability) => (
+            <li key={ability.ability.name}>{ability.ability.name}</li>
+          ))}
+        </ul>
+
+        <h3>Statistiche:</h3>
+        <ul>
+          {pokemon.stats.map((stat) => (
+            <li key={stat.stat.name}>
+              {stat.stat.name}: {stat.base_stat}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default PokemonDetails;
+
